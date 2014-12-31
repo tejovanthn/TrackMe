4ef3f268a1450eaccb6a25d287e8ee5d73f8d142
Tejovanth N <tejovanth.n@gmail.com>
2014-12-31 21:34:47 +0530

    dummy data

--- app/views.py
+++ app/views.py
@@ -8,4 +8,16 @@ from app import app
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html')
+    goal = {'Running': {
+        'current': 2.5,
+        'MIN': 0,
+        'MAX': 1000,
+        'percent': 2.5 * 100 / 1000,
+        }, 'Cycling': {
+        'current': 0,
+        'MIN': 0,
+        'MAX': 1000,
+        'percent': 0 * 100 / 1000,
+        }}
+
+    return render_template('index.html', goal=goal)
