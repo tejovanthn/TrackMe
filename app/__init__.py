54ab3daf439e00912ba929f2f1af07d96d17b5cd
Tejovanth N <tejovanth.n@gmail.com>
2015-01-01 16:33:14 +0530

    moment.js in notes

--- app/__init__.py
+++ app/__init__.py
@@ -9,13 +9,16 @@ from flask.ext.cache import Cache
 
 app = Flask(__name__)
 
-AppConfig(app, configfile="config.py")
+AppConfig(app, configfile='config.py')
 
 Bootstrap(app)
 
 cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 db = SQLAlchemy(app)
+
+from momentjs import momentjs
+app.jinja_env.globals['momentjs'] = momentjs
 
 from app import views, models
 
